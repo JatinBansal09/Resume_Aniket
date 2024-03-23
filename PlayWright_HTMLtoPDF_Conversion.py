b66fbbe84355f254c5af8061bbc4087128ee279f
Jatin Bansal <134193834+JatinBansal09@users.noreply.github.com>
2024-03-23 15:02:08 +0530

    Add files via upload
    The code changes the HTML and CSS code into a pdf file using Playwright.

---
+++ PlayWright_HTMLtoPDF_Conversion.py
@@ -0,0 +1,26 @@
+from playwright.sync_api import sync_playwright
+import os
+import pathlib
+
+filepath = os.path.abspath("F:\Study\Tslas-DESKTOP-G14D31O\Year 4\Sem 8\Online Courses\HTML and CSS\HTML\Projects\Resume_Aniket\Index.html")
+fileUrl= pathlib.Path(filepath).as_uri()
+
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto(fileUrl)
+    page.emulate_media(media="screen")
+    
+    # Set the PDF generation options
+    pdf_options = {
+        'path': "F:\Study\Tslas-DESKTOP-G14D31O\Year 4\Sem 8\Online Courses\HTML and CSS\HTML\Projects\Aniket_resume.pdf",
+        'format': 'A4',  # Specify the page format
+        'prefer_css_page_size': True,  # Use CSS page size definitions
+        'print_background': True,  # Print background colors and images
+        'width':'800px'
+    }
+    
+    # Generate the PDF with specified options
+    page.pdf(**pdf_options)
+    
+    browser.close()
